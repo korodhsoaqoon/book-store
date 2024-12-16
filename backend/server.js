a9388f7b92b5599daa5eef4c4164433fc7f20e6e
korodhsoaqoon <korodhsoaqoon1@gmail.com>
2024-12-16 09:00:44 +0300

    created boiler plate for api routes

--- backend/server.js
+++ backend/server.js
@@ -6,3 +6,27 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+// Routes
+
+app.get("/", (request, response) =>
+  response.send({ message: "API is working" })
+);
+
+// GET - All books
+app.get("/api/books", (req, res) => res.send({ message: "All Books" }));
+
+// GET - Fetch single book /api/books/id
+app.get("/api/books/:id", (req, res) => res.send({ message: "Single Book" }));
+
+// POST - Save a Book /api/books
+app.post("/api/books", (req, res) => res.send({ message: "Save a book" }));
+
+// PUT - Update a book /api/books/:id
+
+app.put("/api/books/:id", (req, res) => res.send({ message: "Update a book" }));
+
+// DELETE Book - /api/books/:id
+app.delete("/api/books/:id", (req, res) =>
+  res.send({ message: "Delete a Book" })
+);
