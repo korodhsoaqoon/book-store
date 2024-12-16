6c7ded708fa5e929b2472c692225dbdf458d2c2a
korodhsoaqoon <korodhsoaqoon1@gmail.com>
2024-12-16 09:55:36 +0300

    created post api end-point

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Book from "./models/book.model.js";
 dotenv.config();
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   connectDB();
@@ -22,7 +24,31 @@ app.get("/api/books", (req, res) => res.send({ message: "All Books" }));
 app.get("/api/books/:id", (req, res) => res.send({ message: "Single Book" }));
 
 // POST - Save a Book /api/books
-app.post("/api/books", (req, res) => res.send({ message: "Save a book" }));
+app.post("/api/books", async (req, res) => {
+  // let's get the data from the red.body - data we want to save
+  const book = req.body;
+
+  // let's check if all fields are filled
+  if (!book.title || !book.price || !book.author || !book.language) {
+    return res
+      .status(400)
+      .json({ sucess: false, message: "Please fill all the fields" });
+  }
+
+  // let's save the book
+  const newBook = new Book(book);
+  try {
+    await newBook.save();
+    res.status(201).send({
+      success: true,
+      data: newBook,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: `Error - ${error.message}` });
+  }
+});
 
 // PUT - Update a book /api/books/:id
 
