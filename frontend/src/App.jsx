84a122ad95acc9d3cc9a09ef895c8745e79c7a48
korodhsoaqoon <korodhsoaqoon1@gmail.com>
2024-12-16 11:49:17 +0300

    installed and configured react-router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import NavBar from "./components/NavBar.jsx";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import AddBookPage from "./pages/AddBookPage.jsx";
 
 const App = () => {
   return (
     <div className="font-mono">
       <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/add-book" element={<AddBookPage />} />
+      </Routes>
     </div>
   );
 };
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,23 +1,30 @@
 import { FaRegSquarePlus } from "react-icons/fa6";
 import { FaHome } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <div className="bg-blue-100">
       <div className="container mx-auto flex justify-between items-center p-2">
-        <h1 className="text-3xl  font-bold bg-gradient-to-r from-cyan-400 to-blue-950 bg-clip-text text-transparent">
-          BookStore
-        </h1>
+        <Link to="/">
+          <h1 className="text-3xl  font-bold bg-gradient-to-r from-cyan-400 to-blue-950 bg-clip-text text-transparent">
+            BookStore
+          </h1>
+        </Link>
         <div>
-          <button className="text-lg mr-2 border-2 border-blue-200 p-2 rounded-lg">
-            <span className="flex items-center gap-2">
-              <FaHome className="inline-block" /> Home
-            </span>
-          </button>{" "}
-          <button className="text-lg mr-2 border-2 border-blue-200 p-2 rounded-lg">
-            <span className="flex items-center gap-2">
-              <FaRegSquarePlus className="inline-block" /> Add Book
-            </span>
-          </button>
+          <Link to="/">
+            <button className="text-lg mr-2 border-2 border-blue-200 p-2 rounded-lg">
+              <span className="flex items-center gap-2">
+                <FaHome className="inline-block" /> Home
+              </span>
+            </button>
+          </Link>
+          <Link to="/add-book">
+            <button className="text-lg mr-2 border-2 border-blue-200 p-2 rounded-lg">
+              <span className="flex items-center gap-2">
+                <FaRegSquarePlus className="inline-block" /> Add Book
+              </span>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
