782d7214216390c665070c13ed2f8321b895a5f8
korodhsoaqoon <korodhsoaqoon1@gmail.com>
2024-12-16 16:14:26 +0300

    Done Book Card Component

---
+++ frontend/src/components/BookCard.jsx
@@ -0,0 +1,44 @@
+import { FiTrash, FiEdit } from "react-icons/fi";
+import { CgMoreO } from "react-icons/cg";
+const BookCard = ({ book }) => {
+  return (
+    <div className="bg-blue-50 flex-1 p-4 rounded-xl shadow-md">
+      <img
+        className="w-full rounded-lg "
+        src={book.cover_image}
+        alt={book.title}
+      />
+      <h2 className="text-2xl font-bold text-blue-800 flex justify-between py-2 ">
+        <span>Title: {book.title}</span>
+        <span>Price: {book.price}</span>
+      </h2>
+      <hr className="bg-blue-100 h-[2px]" />
+      <div>
+        <h2 className="text-lg text-blue-900 ">Author:{book.author}</h2>
+        <h2 className="text-lg text-blue-900 ">Language:{book.language}</h2>
+        <h2 className="text-lg text-blue-900 ">Uploaded:{book.createdAt}</h2>
+        <h2 className="text-lg text-blue-900 ">Updated:{book.updatedAt}</h2>
+        <hr />
+        <div className="flex items-center gap-2">
+          <button className="text-xl mr-2 text-red-700 hover:text-red-800 transition-all duration-200 hover:-translate-y-1">
+            <span className="flex items-center gap-2">
+              <CgMoreO className="inline-block" /> More
+            </span>
+          </button>
+          <button className="text-xl mr-2 text-red-700 hover:text-red-800 transition-all duration-200 hover:-translate-y-1">
+            <span className="flex items-center gap-2">
+              <FiEdit className="inline-block" /> Edit
+            </span>
+          </button>
+          <button className="text-xl mr-2 text-red-700 hover:text-red-800 transition-all duration-200 hover:-translate-y-1">
+            <span className="flex items-center gap-2">
+              <FiTrash className="inline-block" /> Delete
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BookCard;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import { useBookStore } from "../store/book.store";
+import BookCard from "../components/BookCard";
+import { Link } from "react-router-dom";
 const HomePage = () => {
   const { getBooks, books } = useBookStore();
 
@@ -7,8 +9,23 @@ const HomePage = () => {
     getBooks();
   }, []);
 
-  console.log(books);
-  return <div>HomePage</div>;
+  return (
+    <div className="container mx-auto p-2 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      <h1 className="text-4xl text-center my-5 font-bold text-blue-900 italic">
+        All Books 📚
+      </h1>
+      {!books.length > 0 ? (
+        <h1 className="text-center text-2xl  font-semibold italic">
+          There is no book currently Available
+          <Link className="ml-2 text-blue-900  underline" to="/add-book">
+            Add New Book
+          </Link>
+        </h1>
+      ) : (
+        books.map((book) => <BookCard book={book} />)
+      )}
+    </div>
+  );
 };
 
 export default HomePage;
