082db31f5d38734d216414651ea93cf2680810fa
korodhsoaqoon <korodhsoaqoon1@gmail.com>
2024-12-17 11:00:49 +0300

    prepared for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     const connection = await mongoose
-      .connect(process.env.LOCAL_MONGO_DB_URI)
+      .connect(process.env.MONGO_ATLAS_DB_URI)
       .then(() => console.log("Connected to the MongoDB"));
   } catch (error) {
     console.log(error.message);
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import Book from "./models/book.model.js";
 import booksRouter from "./routes/book.route.js";
 import cors from "cors";
+import path from "path";
 dotenv.config();
 const app = express();
 app.use(cors());
@@ -12,6 +12,14 @@ app.use(express.json());
 app.use("/api/books", booksRouter);
 
 const PORT = process.env.PORT || 3000;
+
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server is running at http://localhost:${PORT}`);
